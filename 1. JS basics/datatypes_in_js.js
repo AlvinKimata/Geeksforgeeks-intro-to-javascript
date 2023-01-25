788aa01e8f45bb6e1841a0ffd3fdca4d4e16f999
Alvin Kimata <alvinkimata@gmail.com>
2023-01-25 11:33:27 +0300

    upload dtypes in js

---
+++ 1. JS basics/datatypes_in_js.js
@@ -0,0 +1,20 @@
+//String dtype
+var txt = "Geeksforgeeks"
+
+//Boolean dtype.
+var bool = (1 == 3)
+
+//Store array.
+var places = ['GFG', 'Computer', 'Hello']
+
+//Store object data.
+var Student = {
+    firstName: 'John',
+    lastName: 'Diaz',
+    age: 35, 
+    mark:'blueEYE'
+}
+
+console.log(txt ,'\n', places)
+
+console.log(Student)
---
+++ 1. JS basics/variables_in_js.js
@@ -0,0 +1,25 @@
+//Variable declaration.
+var c, d, e;
+
+//Assign value to the variable.
+c = 5;
+
+//Compute value of variables.
+d = c;
+e = c / d;
+
+
+var Name="Apple"
+
+//Function definition.
+function MyFunction(){
+    var num = 45;
+
+    //Display the value of the global variable.
+    console.log(Name);
+
+    //Display the value of the local variable.
+    console.log("<br>" + num);
+}
+
+MyFunction()
