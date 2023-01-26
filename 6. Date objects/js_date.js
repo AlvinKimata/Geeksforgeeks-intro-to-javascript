c56ae22c493b7f3eee8aa1d92e041934c82a92f7
Alvin Kimata <alvinkimata@gmail.com>
2023-01-26 11:57:21 +0300

    date objects in js

---
+++ 6. Date objects/js_date.js
@@ -0,0 +1,13 @@
+// Javascript Date object in JavaScript is used to represent a moment in time.
+var A = new Date();
+
+console.log(A + '\n');
+
+// When a date String is passed as the parameter then it returns the same as the parameter including the day name. 
+var A = new Date("June 22, 1985 07:19:35");
+
+console.log(A + '\n');
+
+//When some numbers are passed as parameters then they are considered as year, month, day, hours, minutes, seconds, and milliseconds respectively.
+var A = new Date(1996, 10, 13, 5, 30, 22);
+console.log(A + '\n');
