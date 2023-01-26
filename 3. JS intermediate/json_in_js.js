d6317b933012fb10972ac6e262c78c9cfbe623e2
Alvin Kimata <alvinkimata@gmail.com>
2023-01-26 07:26:29 +0300

    include a js file in another js file.

---
+++ 3. JS intermediate/json_in_js.js
@@ -0,0 +1,28 @@
+//JSON or javascript object notation is a format for structuring data.
+//Data is stored in name/value pairs.
+
+var person = {
+    'name': 'Thanos',
+    'occupation': 'Destroy half of humanity'
+}
+
+
+var avengers = {
+        "Tony" : {
+          "Name" : "Tony stark",
+          "also known as" : "Iron man",
+          "Abilities" : [ "Genius", "Billionaire",
+                        "Playboy", "Philanthropist" ]
+        },
+  
+       "Parker"  : {
+          "Name" : "Peter parker",
+          "also known as" : "Spider man",
+          "Abilities" : [ "Spider web", "Spidy sense" ]
+        }
+};
+
+
+console.log(person)
+
+console.log(avengers)
